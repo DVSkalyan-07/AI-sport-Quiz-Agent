93cd1842cf72a1b9187d48f1e8bc786022549e03
Desaboyina venkata sai kalyan <dvskalyan07@gmail.com>
2026-07-20 09:16:44 +0530

    Create src.__init__.py

---
+++ src.__init__.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+# Load variables from .env file
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if not OPENAI_API_KEY:
+    print("[WARNING]: API Key is missing. Check your .env file setup!")
